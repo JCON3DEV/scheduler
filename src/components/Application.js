9d47689b90008c91029e06475766df83c98da453
Jordan <jconheasman@gmail.com>
2020-09-01 16:38:12 -0700

    added the interviewer list item and styles. also the necessary stories.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,13 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+import InterviewerListItem from "components/InterviewerListItem";
 import "components/Application.scss";
 
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
+  const [state, setState] = useState("Monday")
   return (
     <main className="layout">
       <section className="sidebar">
-        {
-          <>
+        {/* <nav><DayList
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
+        />
+        </nav>  */}
+            <nav>
+               <DayList
+                days={days}
+                day={state}
+                setDay={setState}/>
+            </nav>
             <img
               className="sidebar--centered"
               src="images/logo.png"
@@ -20,12 +52,12 @@ export default function Application(props) {
               src="images/lhl.png"
               alt="Lighthouse Labs"
             />
-          </>
-        }
       </section>
-      <body>
-        <h1>This is my scheduler</h1>
-      </body>
+      <h1>This is my scheduler</h1>
+      <h2>{Application}</h2>
+      <div>
+        {InterviewerListItem}
+      </div>
 
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
---
+++ src/components/Appointment.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "components/application.css";
+import classnames from "classnames";
+
+export default function Appointment(props){
+
+  return <p>Where does this display?</p>
+}
