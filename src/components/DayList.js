80fb9e2d2757d6efd8f7d3c9b365aa7fd0f15cb5
Jordan <jconheasman@gmail.com>
2020-09-01 14:20:27 -0700

    added DayList object and DayList item as well as stories for testing

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,6 +1,31 @@
 import React from "react";
 import classnames from "classnames";
+import DayListItem from "./DayListItem";
 
 export default function DayList(props){
-  return <ul></ul>
+  console.log("props; ", props);
+
+  const daylist = props.days.map((day) => 
+      { 
+        return<DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}/>
+      }) 
+  return <ul>{daylist}</ul>
 }
+// export default function DayList(props) {
+//   console.log("props; ", props);
+//   const daylist = props.days.map{(day) => {
+//     <DayListItem
+//       key={day.id},
+//       name={day.name},
+//       spots={day.spots},
+//       selected={day.name === props.day},
+//       setDay={props.setDay}
+//     />
+//   }
+//   return <ul>{daylist}</ul>
+// }
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,27 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full" : props.full,
   });
-  
+  const formatSpots= () =>{
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if  (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    if (props.spots === 2) {
+      return "2 spots remaining";
+    }
+    else {
+      return `${props.spots} remaining`;
+    }
+  }
+
   return (
     <li 
       className= {dayClass}
       onClick={() => props.setDay(props.name)}>{/* represents individual days */}
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
