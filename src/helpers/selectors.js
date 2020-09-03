915371c5889d01992c1dcde5329b031665cfcdb5
Jordan <jconheasman@gmail.com>
2020-09-03 15:15:51 -0700

    added a function to return the appoinmtemnt ids of a given day. Also added a Jest test document for this function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,17 @@
 
 
-function getAppointmentsForDay(state, day) {
-  const filteredDays  = state.days.filter(day => days.day === day);
-  return filteredDays;
+export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) {
+    return [];
+  };
+  const filteredDays = state.days.find(dayObject => dayObject.name === day);
+  if (!filteredDays) {
+    return [];
+  }
+  const result = filteredDays.appointments.map( (appointmentId) => {
+    return state.appointments[appointmentId];
+  }); 
+
+  return result;
 }
+
